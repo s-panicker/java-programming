a6e84d0e0acf30406adc5923b54f46eae1c3a89e
s-panicker <sahilanil007@gmail.com>
2025-01-08 20:30:20 +0100

    NumberGuessingGame

--- Introduction/src/NumberGuessingGame.java
+++ Introduction/src/NumberGuessingGame.java
@@ -26,6 +26,7 @@ public class NumberGuessingGame {
                 System.out.println("Congratulations! You guessed the number in " + attempts + " attempts.");
             }
         }
+        //Adding comments
 
         scanner.close();
     }
