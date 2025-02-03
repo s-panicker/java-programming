25522fe315f3c6ac99e4bf9666630b33d15994e8
s-panicker <sahilanil007@gmail.com>
2025-02-03 21:30:20 +0100

    Todo List

---
+++ Intermmediate Concepts/src/TodoList.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class TodoList {
+    private ArrayList<String> tasks;
+
+    // Constructor to initialize tasks list
+    public TodoList() {
+        tasks = new ArrayList<>();
+}
