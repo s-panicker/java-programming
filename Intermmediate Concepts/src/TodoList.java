2b820b24b5c689865304dbe563b6a26d9521504a
s-panicker <sahilanil007@gmail.com>
2025-02-04 22:30:20 +0100

    Todo List

--- Intermmediate Concepts/src/TodoList.java
+++ Intermmediate Concepts/src/TodoList.java
@@ -7,3 +7,18 @@ public class TodoList {
     public TodoList() {
         tasks = new ArrayList<>();
 }
+    // Method to add a task
+    public void addTask(String task) {
+        tasks.add(task);
+        System.out.println("Task added: " + task);
+    }
+
+    // Method to remove a task by index
+    public void removeTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            System.out.println("Task removed: " + tasks.get(index));
+            tasks.remove(index);
+        } else {
+            System.out.println("Invalid task index.");
+        }
+    }
