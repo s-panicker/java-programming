b9b8e19fa0273f0bee5f631050ce7b15452a7bc7
s-panicker <sahilanil007@gmail.com>
2025-05-05 14:42:00 +0200

    Refactor data handling logic

--- Projects/theFinalBoss/src/main/GamePanel.java
+++ Projects/theFinalBoss/src/main/GamePanel.java
@@ -1,4 +1,51 @@
 package main;
 
-public class GamePanel {
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class GamePanel extends JPanel {
+
+    private MouseInputs mouseInputs;
+    private int xDelta = 100, yDelta = 100;
+    private int frames;
+
+    public GamePanel() {
+        mouseInputs = new MouseInputs(this);
+        addKeyListener(new KeyboardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
+
+    }
+
+    public void changeXDelta(int value) {
+        this.xDelta += value;
+        repaint();
+    }
+
+    public void changeYDelta(int value) {
+        this.yDelta += value;
+        repaint();
+    }
+
+    public void setRectPos(int x, int y) {
+        this.xDelta = x;
+        this.yDelta = y;
+        repaint();
+    }
+    public void paintComponent (Graphics g) {
+        super.paintComponent(g);
+        g.fillRect( xDelta, yDelta,200,50);
+        frames++;
+        repaint();
+
+
+    }
+
+
+
 }
