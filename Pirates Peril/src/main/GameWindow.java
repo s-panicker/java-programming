3343d207c2f2f116866910bec8a0c5574a910e82
s-panicker <sahilanil007@gmail.com>
2025-06-23 08:30:00 +0200

    Finished changes to UI

--- Pirates Peril/src/main/GameWindow.java
+++ Pirates Peril/src/main/GameWindow.java
@@ -1,20 +1,38 @@
 package main;
 
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
+
 import javax.swing.JFrame;
 
 public class GameWindow {
-    private JFrame jframe;
+	private JFrame jframe;
 
-    public GameWindow(GamePanel gamePanel) {
+	public GameWindow(GamePanel gamePanel) {
 
-        jframe = new JFrame();
+		jframe = new JFrame();
 
-        jframe.setSize(400, 400);
-        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jframe.add(gamePanel);
-        jframe.setVisible(true);
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.add(gamePanel);
+		
+		jframe.setResizable(false);
+		jframe.pack();
+		jframe.setLocationRelativeTo(null);
+		jframe.setVisible(true);
+		jframe.addWindowFocusListener(new WindowFocusListener() {
 
+			@Override
+			public void windowLostFocus(WindowEvent e) {
+				gamePanel.getGame().windowFocusLost();
+			}
 
-    }
+			@Override
+			public void windowGainedFocus(WindowEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+	}
 
 }
